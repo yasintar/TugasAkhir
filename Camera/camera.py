9f26b763531f13e91d0295c1a895eab446da2960
EdoYogatama <roronoadezoro@gmail.com>
2022-02-06 17:32:31 +0700

    Add camera class to test

---
+++ Camera/camera.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+class Cam:
+    def __init__(self) -> None:
+        self.cap = cv.VideoCapture(0)
+        self.run()
+
+    def run(self):
+        while True:
+            ret, frame = self.cap.read()
+
+            cv.imshow('Stream', frame)
+            k = cv.waitKey(5) & 0xFF
+            if k == 27:
+                break
+        self.stop()
+
+    def stop(self):
+        self.cap.release()
+        cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    camera = Cam()
