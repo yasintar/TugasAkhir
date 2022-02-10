4b8b957616f49efb6cfb55ebd043eb726f867d78
EdoYogatama <roronadezoro@gmail.com>
2022-02-10 17:41:03 +0700

    capture done

--- camera.py
+++ camera.py
@@ -1,4 +1,7 @@
+from logging import exception
 import cv2 as cv
+from datetime import datetime
+import os
 
 class Cam:
     def __init__(self) -> None:
@@ -15,5 +18,21 @@ class Cam:
         self.cap.release()
         cv.destroyAllWindows()
 
+    def capture(self):
+        print("Capture")
+        now = datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
+        name = "./images/{}.png".format(now)
+        if self.frame is not None:
+            if not cv.imwrite(name, self.frame):
+                raise Exception('Could not write image')
+
+    def showImg(self):
+        path = os.path.join(os.getcwd(), 'images', 'Screenshot from 2022-01-12 16-10-15.png')
+        while True:
+            img = cv.imread(path)
+
+            cv.imshow('img', img)
+
 if __name__ == "__main__":
     camera = Cam()
+    camera.showImg()
--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 from camera import Cam
 import cv2 as cv
-from datetime import datetime
-import time
 
 class Main:
     def __init__(self) -> None:
         self.camera = Cam()
-        self.frame = None
-        self.starttime = time.time()
+        self.run()
 
     def run(self):
         while True:
             self.camera.start()
-            self.frame = self.camera.frame
 
-            self.captureFrame()
+            self.camera.capture()
 
             k = cv.waitKey(5) & 0xFF
             if k == 27:
                 break
         self.camera.stop()
 
-    def captureFrame(self):
-        print("Capture")
-        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        if self.frame is not None:
-            cv.imwrite("{}.jpg".format(now), self.frame)
-
 if __name__=="__main__":
     main = Main()
-    main.run()
