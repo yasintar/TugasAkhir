9d09b6798ee17b033fee65b6a4c1aec9084ffd32
EdoYogatama <roronadezoro@gmail.com>
2022-02-14 17:34:49 +0700

    saving pic success

--- camera.py
+++ camera.py
@@ -1,7 +1,6 @@
 from logging import exception
 import cv2 as cv
 from datetime import datetime
-import os
 
 class Cam:
     def __init__(self) -> None:
@@ -20,19 +19,11 @@ class Cam:
 
     def capture(self):
         print("Capture")
-        now = datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
+        now = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
         name = "./images/{}.png".format(now)
         if self.frame is not None:
             if not cv.imwrite(name, self.frame):
                 raise Exception('Could not write image')
 
-    def showImg(self):
-        path = os.path.join(os.getcwd(), 'images', 'Screenshot from 2022-01-12 16-10-15.png')
-        while True:
-            img = cv.imread(path)
-
-            cv.imshow('img', img)
-
 if __name__ == "__main__":
     camera = Cam()
-    camera.showImg()
