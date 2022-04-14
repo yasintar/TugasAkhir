02922f99ea3e25f8bcf44d7967fe3fe59007bbb8
EdoYogatama <roronoadezoro@gmail.com>
2022-04-15 00:37:52 +0700

    debug

--- constant.py
+++ constant.py
@@ -1,3 +1,3 @@
 DEVICEDEBUGCAMERA   = 0
-DEVICESTGCAMERA     = 'rstp://admin:cctv1234@192.168.1.100:554/Streaming/channels/101'
-TIME            = 10
+DEVICESTGCAMERA     = 'rstp://admin:cctv1234@192.168.1.100:554/Streaming/Channels/101'
+TIME                = 10
