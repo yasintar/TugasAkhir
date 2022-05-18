9b449d0fcb50934f57ab5c0bc09efcad14db791d
EdoYogatama <roronoadezoro@gmail.com>
2022-05-18 19:52:20 +0700

    edit constant vale

--- constant.py
+++ constant.py
@@ -2,10 +2,10 @@ DEVICEDEBUGCAMERA   = 0
 DEVICESTGCAMERA     = 'rtsp://admin:cctv1234@192.168.1.100:554/h264Preview_01_main'
 TIME                = 10
 
-YOLO_SCALE = 0.00392
-YOLO_IMGSIZE = (416,416)
-YOLO_CONFI = 0.45
+YOLO_SCALE          = 0.00392
+YOLO_IMGSIZE        = (416,416)
+YOLO_CONFI          = 0.45
 
-CONST_CPU = 50      #Batas usage CPU jika lebih do something
-CONST_RAM = 80      #Batas usage RAM jika lebih do something
-CONST_DISK = 80     #Batas usage Disk jika lebih do something
+CONST_CPU           = 50      #Batas usage CPU jika lebih do something
+CONST_RAM           = 80      #Batas usage RAM jika lebih do something
+CONST_DISK          = 80     #Batas usage Disk jika lebih do something
