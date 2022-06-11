453f22926fcaa76f2dc93dc73eeee1929f0b1485
EdoYogatama <roronoadezoro@gmail.com>
2022-06-11 19:06:48 +0700

    debug available

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from ast import arg
 from camera import Cam
 from yolo import YoloHandler
 from ags import AGS
 from relay import Relay
+
+import argparse
 import functools
 
 class Main:
-    def __init__(self) -> None:
-        self.camera = Cam(debug=True)
-        self.yolodetector = YoloHandler()
-        self.ags = AGS(debug=False)
-        self.relay = Relay()
+    def __init__(self, debug) -> None:
+        if debug:
+            self.camera = Cam(debug=True)
+            self.yolodetector = YoloHandler()
+            self.ags = AGS(debug=False)
+            self.relay = None
+        else:
+            self.camera = Cam(debug=False)
+            self.yolodetector = YoloHandler()
+            self.ags = AGS(debug=False)
+            self.relay = Relay()
 
         self.runable = True
         self.isRelayOn = False
@@ -23,6 +32,9 @@ class Main:
 
         self.camera.setTimeToCapture(0)
         self.camera.captureThread.start()
+
+        if self.relay is not None:
+            self.relay.start()
         
         try:
             while self.runable:
@@ -45,14 +57,17 @@ class Main:
         if not self.runable:
             self.stop()
             self.runable = True
-            self.ags.run()
             self.start.cache_clear()
             self.start()
 
     def stop(self):
         self.ags.stop()
+        self.yolodetector.stop()
         self.camera.stop()
         self.yolodetector.stop()
+        if self.relay is not None: self.relay.stop()
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="to debug with webcam")
     main = Main()
