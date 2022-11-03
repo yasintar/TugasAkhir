864997baedf3ccffe665af5d4d3736d82e5f382f
EdoYogatama <roronoadezoro@gmail.com>
2022-11-03 11:31:16 +0700

    edit constant threshold

--- constant.py
+++ constant.py
@@ -11,8 +11,8 @@ YOLO_WEIGHT         = './model/yolo-obj_last.weights'
 YOLO_CFG            = './model/yolo-obj.cfg'
 
 FULL_RESOURCE       = 100
-CONST_CPU           = 80      #Batas usage CPU jika lebih do something
-CONST_RAM           = 80      #Batas usage RAM jika lebih do something
+CONST_CPU           = 60.77      #Batas usage CPU jika lebih do something
+CONST_RAM           = 40.42      #Batas usage RAM jika lebih do something
 CONST_DISK          = 80      #Batas usage Disk jika lebih do something
 
 MAX_RELAY_LIST      = 5
