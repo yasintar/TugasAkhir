1cdb7b76939417bef25e9d83f0955b46bb84d8df
EdoYogatama <roronoadezoro@gmail.com>
2022-12-13 18:23:48 +0700

    edit tem sleep

--- integerDataReader.py
+++ integerDataReader.py
@@ -21,7 +21,7 @@ class Reader:
 
             if self.isStopped : break
 
-            time.sleep(1)
+            time.sleep(0.25)
 
     def start(self):
         print("[]\tStart to read a File as Input")
